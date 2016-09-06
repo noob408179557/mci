ea784b53b199e271edeac3d12fec0f7be31ef3e3
addison <408179557@qq.com>
2016-09-06 09:22:18 +0800

    r444

---
+++ WebRoot/js/invoice/detailCommon.js
@@ -0,0 +1,40 @@
+
+
+
+function switchItem(itemName){
+	switch(itemName){
+	case "Overtimes10X":return "Overtimes 1.0X";break;
+	case "Overtimes15X":return "Overtimes 1.5X";break;
+	case "Overtimes20X":return "Overtimes 2.0X";break;
+	case "CPF":return "CPF";break;
+	case "SDF":return "SDF";break;
+	case "WICA2":return "WICA 2%";break;
+	case "MedicalCoverageFee2":return "Medical Coverage Fee 2%";break;
+	case "AdminFee":return "Admin Fee";break;
+	case "TransportAllowance":return "Transport Allowance";break;
+	case "LaundryAllowance":return "Laundry Allowance";break;
+	case "AfternoonShiftAllowance":return "Afternoon Shift Allowance";break;
+	case "MedicalReimbursement":return "Medical Reimbursement";break;
+	case "MileageERPReimbursement":return "Mileage & ERP Reimbursement";break;
+	case "LessUnpaidLeave":return "Less: Unpaid Leave";break;
+	case "TransportReimbursement":return "Transport Reimbursement";break;
+	case "MealAllowance":return "Meal Allowance";break;
+	case "AttendanceAllowance":return "Attendance Allowance";break;
+	case "Allowance":return "Allowance";break;
+	case "PublicHolidayOT":return "Public Holiday OT";break;
+	case "Lateness":return "Lateness";break;
+	case "AnnualLeaveEncashment":return "Annual Leave Encashment";break;
+	case "Bonus":return "Bonus";break;
+	case "CompletionBonus":return "Completion Bonus";break;
+	case "Incentive":return "Incentive";break;
+	case "BackPaySalary":return "Back Pay Salary";break;
+	case "BackPayOvertimes":return "Back Pay Overtimes";break;
+	case "AdjustmentSalary":return "Adjustment Salary";break;
+	case "AdjustmentOvertimes":return "Adjustment Overtimes";break;
+	case "SalaryInLieu":return "Salary In Lieu";break;
+	case "PaidAnnualLeave":return "Paid Annual Leave";break;
+	case "PaidMedicalLeave":return "Paid Medical Leave";break;
+	case "PaidChildcareLeave":return "Paid Childcare Leave";break;
+	case "LessMidMonthSalary":return "Less: Mid Month Salary";break;
+	}
+}
